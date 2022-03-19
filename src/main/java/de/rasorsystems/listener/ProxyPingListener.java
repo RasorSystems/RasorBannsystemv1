9334d749fbd30d88c08d434212c2b0d1245e3bca
nicoc <capparozzanico@gmail.com>
2022-03-19 22:44:20 +0100

    added src

--- src/main/java/de/rasorsystems/listener/ProxyPingListener.java
+++ src/main/java/de/rasorsystems/listener/ProxyPingListener.java
@@ -19,21 +19,22 @@ public class ProxyPingListener implements Listener
     
     @EventHandler
     public void onPing(final ProxyPingEvent e) {
-        final ServerPing ping = e.getResponse();
-        final ServerPing.Players players = ping.getPlayers();
-        final ServerPing.Protocol version = ping.getVersion();
-        if (this.main.getConfiguration().getBoolean("maintenancestatus")) {
-            version.setName(Main.getInstance().getConfiguration().getString("maintenenance-version").replace("&", "§"));
-            version.setProtocol(2);
-            players.setSample(new ServerPing.PlayerInfo[] { new ServerPing.PlayerInfo(this.main.getConfiguration().getString("motd-hover").replace("&", "§"), UUID.randomUUID()) });
-            ping.setDescription(this.main.getConfiguration().getString("maintenancemotd-1").replace("&", "§") + "\n" + this.main.getConfiguration().getString("maintenancemotd-2").replace("&", "§"));
-            e.setResponse(ping);
-        }
-        else {
-            version.setName("§7" + this.main.getConfiguration().getInt("max_player") + "§8/§7" + e.getConnection().getListener().getMaxPlayers());
-            players.setSample(new ServerPing.PlayerInfo[] { new ServerPing.PlayerInfo(this.main.getConfiguration().getString("motd-hover").replace("&", "§"), UUID.randomUUID()) });
-            ping.setDescription(this.main.getConfiguration().getString("motd-1").replace("&", "§") + "\n" + this.main.getConfiguration().getString("motd-2").replace("&", "§"));
-            e.setResponse(ping);
+        if (Main.getInstance().getConfiguration().getBoolean("usemotd")) {
+            final ServerPing ping = e.getResponse();
+            final ServerPing.Players players = ping.getPlayers();
+            final ServerPing.Protocol version = ping.getVersion();
+            if (this.main.getConfiguration().getBoolean("maintenancestatus")) {
+                version.setName(Main.getInstance().getConfiguration().getString("maintenenance-version").replace("&", "§"));
+                version.setProtocol(2);
+                players.setSample(new ServerPing.PlayerInfo[]{new ServerPing.PlayerInfo(this.main.getConfiguration().getString("motd-hover").replace("&", "§"), UUID.randomUUID())});
+                ping.setDescription(this.main.getConfiguration().getString("maintenancemotd-1").replace("&", "§") + "\n" + this.main.getConfiguration().getString("maintenancemotd-2").replace("&", "§"));
+                e.setResponse(ping);
+            } else {
+                version.setName("§7" + this.main.getConfiguration().getInt("max_player") + "§8/§7" + e.getConnection().getListener().getMaxPlayers());
+                players.setSample(new ServerPing.PlayerInfo[]{new ServerPing.PlayerInfo(this.main.getConfiguration().getString("motd-hover").replace("&", "§"), UUID.randomUUID())});
+                ping.setDescription(this.main.getConfiguration().getString("motd-1").replace("&", "§") + "\n" + this.main.getConfiguration().getString("motd-2").replace("&", "§"));
+                e.setResponse(ping);
+            }
         }
     }
 }
